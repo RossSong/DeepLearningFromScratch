85c7f8b94c7fff8a5fea852fc43c78b677559b97
hgsong <hgsong@wantedlab.com>
2017-12-28 11:20:22 +0900

    Relu 코드 개선

--- testDeepLearning/Network2.swift
+++ testDeepLearning/Network2.swift
@@ -73,14 +73,18 @@ func numericalGradient(f: ((_ x: Tensor<Double>) -> Double), x: Tensor<Double>) 
 class Relu {
     var dx: Tensor<Double>?
     
+    func mask(out: Tensor<Double>, i: Int, j: Int) {
+        if out.elements[i * out.dimensions[1] + j] < 0 {
+            out.elements[i * out.dimensions[1] + j] = 0
+        }
+    }
+    
     func forward(x: Tensor<Double>) -> Tensor<Double> {
         let out = x.copy()
         
         for i in 0..<out.dimensions[0] {
             for j in 0..<out.dimensions[1] {
-                if out.elements[i * out.dimensions[1] + j] <= 0 {
-                    out.elements[i * out.dimensions[1] + j] = 0
-                }
+                mask(out: out, i: i, j: j)
             }
         }
         
@@ -90,9 +94,7 @@ class Relu {
     func backward(dout: Tensor<Double>) -> Tensor<Double> {
         for i in 0..<dout.dimensions[0] {
             for j in 0..<dout.dimensions[1] {
-                if dout.elements[i * dout.dimensions[1] + j] <= 0 {
-                    dout.elements[i * dout.dimensions[1] + j] = 0
-                }
+                mask(out: dout, i: i, j: j)
             }
         }
 
